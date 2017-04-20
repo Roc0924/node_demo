106b682996f4583c952329736444de65d8366165
wangzhenpeng <wangzhenpeng@gomeplus.com>
2017-04-20 13:57:40 +0800

    Assert

---
+++ offical.example/AssertionTesting/Assert.js
@@ -0,0 +1,17 @@
+/**
+ * Create with IntelliJ IDEA
+ * Author               : wangzhenpeng
+ * Date                 : 2017/4/20
+ * Time                 : 11:45
+ * Description          : AssertionTesting
+ */
+
+const assert = require('assert');
+
+assert(true);
+
+assert(1);
+
+assert(0);
+
+assert(false, 'it\'s false');
